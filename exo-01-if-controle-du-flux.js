3c73d1f8eae92e5cb8ece7659966499940cde309
pierre pracht <pierre.pracht@gmail.com>
2022-10-23 17:18:52 +0200

    début exo (exemple)

---
+++ exo-01-if-controle-du-flux.js
@@ -0,0 +1,25 @@
+/**
+ * Faire un simple if pour vérifier si une variable est égale à 1
+ */
+
+// Déclaration de la variable
+const a = 1;
+
+// Si la variable est égale à 1
+if (a === 1) {
+  // Afficher le message
+  console.log("a est égale à 1");
+} else {
+  // Sinon  (si la variable n'est pas égale à 1)
+  // Afficher le message
+  console.log("a n'est pas égale à 1");
+}
+
+/**
+ * Testez si ("0" == 0) est vrai ou faux
+ */
+
+/**
+ * Testez si ("0" === 0) est vrai ou faux
+ * // === est un opérateur de comparaison stricte
+ */
